c76d336d1dd1c3bd05b192aa437e7b299f155323
13psalomshchicovch-svg <13psalomshchicovch@gmail.com>
2026-02-11 03:31:07 +0300

    Creat new branch and test

--- src/AutomaticTesting/GitHubTakes.java
+++ src/AutomaticTesting/GitHubTakes.java
@@ -1,5 +1,15 @@
 package AutomaticTesting;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.IO.println;
+
 public class GitHubTakes {
   int x = 5;
+
+  @Test
+  void doSomeTest(){
+    Assertions.assertTrue(3>2);
+  }
 }
